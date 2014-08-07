d4ccc5dffa2df84b35913e20bba34493861f5cac
Matthew Voss <voss.matthew@gmail.com>
2014-08-06 23:07:29 -0500

    Add a cache busting tag on restarts

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ var fs = require('fs'),
     Workflows = {},
     opts = {},
     config = {},
-    reconnectTries = 0,
+    reconnectTries = 0, cacheBuster = moment().format("X"),
     hmac, signature, connection, client,
     transport, acl, subClient,
     CheckinMemberFieldValues, RegMemberFieldValues, CheckinGroupMembers,
@@ -179,7 +179,7 @@ exports.index = function(req, res){
     if (error) { console.log(error); }
     var prefix = (opts.configs.get("prefix")) ? opts.configs.get("prefix") : "";
     var pageBuilder = handlebars.compile(content),
-        html = pageBuilder({'init':init, 'prefix':prefix});
+        html = pageBuilder({'init':init, 'prefix':prefix, 'cacheBuster': cacheBuster});
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(html, 'utf-8');
